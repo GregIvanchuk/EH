b32c3fe7d53c2d06ebde78ddcd88a37ff703987a
GregIvanchuk <gregiv99@gmail.com>
2023-10-23 19:22:40 +0300

    sd

--- src/app/components/Goods/index.js
+++ src/app/components/Goods/index.js
@@ -48,23 +48,22 @@ scrollTo({ top: 0, behavior: 'smooth' }); // Вгору сторінки з пл
   return (
     <div className={styles.wrapper}>
       <Categor categories ={categories} catId={catId} OnclickCatIndex={(id) => dispatch(getCatId(id))}/>
-     <div className={styles.goods}>
-     {goods.map((item,index) => <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf} callback2={(idd)=> pullOut(idd)}  callback={(idt)=> pullOut(idt)} key={index}  good={...item}/>)}
-     {/* {
-     isLoading ? <Image className={styles.loader} width={500} height={300} src="/Loading.gif" alt='gif'/>:
-     inputValued ? 
-    ( (catId == 0) ? 
-    filtredGoods.map((item,index) => <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf} callback2={(idd)=> pullOut(idd)}  callback={(idt)=> pullOut(idt)} key={index}  good={...item}/>):
-    filtredGoods.filter((obj => (obj.category === catId)))
-     .map((item,index)=> <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf}  callback={(idt)=> pullOut(idt)}  key={index}  good={...item}/>)
-    ):
-    ((catId == 0) ? 
-    currentGoods.map((item,index) => <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf} callback2={(idd)=> pullOut(idd)}  callback={(idt)=> pullOut(idt)} key={index}  good={...item}/>):
-    goods.filter((obj => (obj.category === catId)))
-  .map((item,index)=> <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf}  callback={(idt)=> pullOut(idt)}  key={index}  good={...item}/>)
-    )
-   }     */}
-     </div>
+      <div className={styles.goods}>
+  {
+    isLoading ? <Image className={styles.loader} width={500} height={300} src="/Loading.gif" alt='gif'/>:
+    inputValued ?
+      ((catId == 0) ?
+        filtredGoods.map((item,index) => <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf} callback2={(idd)=> pullOut(idd)}  callback={(idt)=> pullOut(idt)} key={index}  good={...item}/>):
+        filtredGoods.filter((obj => (obj.category === catId)))
+          .map((item,index)=> <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf}  callback={(idt)=> pullOut(idt)}  key={index}  good={...item}/>)
+      ):
+      ((catId == 0) ?
+        currentGoods.map((item,index) => <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf} callback2={(idd)=> pullOut(idd)}  callback={(idt)=> pullOut(idt)} key={index}  good={...item}/>):
+        goods.filter((obj => (obj.category === catId)))
+        .map((item,index)=> <Good isPress={isPress} setIsPress={setIsPress} setIdf={setIdf}  callback={(idt)=> pullOut(idt)}  key={index}  good={...item}/>)
+      )
+  }
+</div>
      <ReactPaginate
           previousLabel={'←'}
           nextLabel={'→'}
